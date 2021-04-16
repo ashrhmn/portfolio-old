356eea49bd77c100f7b375455d54410f1a5d7c46
Ashik Rahman <ashrhmn99@gmail.com>
2021-04-16 10:33:50 +0600

    modified project component

---
+++ src/components/Portfolio/Project.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="m-5">
+    <div class="text-left text-2xl">{{ pData.minTitle }}</div>
+    <div class="text-left text-4xl">{{ pData.title }}</div>
+    <div class="text-left text-lg">{{ pData.intro }}</div>
+
+    <div class="mx-auto flex-container flex-col">
+      <img
+        class="pt-10 assetImage mx-auto"
+        :src="pData.assets[selectedIndex].image"
+      />
+      <div class="text-lg">{{ pData.assets[selectedIndex].desc }}</div>
+    </div>
+
+    <div class="flex w-40 mx-auto">
+      <div
+        v-for="(asset, index) in pData.assets"
+        :key="asset.desc"
+        class="mx-auto"
+      >
+        <button
+          @click="selectedIndex = index"
+          :class="{ 'btn-flex-active': isActive(index) }"
+          class="btn-flex custom-control"
+        ></button>
+      </div>
+    </div>
+
+    <div v-for="content in pData.contents" :key="content">
+      <div class="text-left text-lg">
+        {{ content }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["pData"],
+  methods: {
+    isActive(index) {
+      return index == this.selectedIndex;
+    },
+    changeImage() {
+      if (this.pData.assets.length - 1 == this.selectedIndex) {
+        this.selectedIndex = 0;
+      } else {
+        this.selectedIndex++;
+      }
+    },
+  },
+
+  mounted: function () {
+    this.changeImage();
+    setInterval(this.changeImage, 7000);
+  },
+  data: () => {
+    return {
+      selectedIndex: 0,
+    };
+  },
+};
+</script>
+
+<style>
+.im {
+  min-width: 20rem;
+}
+
+.btn-flex {
+  width: 10px;
+  height: 10px;
+  background-color: hsl(240, 52%, 44%);
+  border-radius: 1rem;
+}
+
+.btn-flex-active {
+  width: 10px;
+  height: 10px;
+  background-color: hsl(28, 85%, 53%);
+  border-radius: 1rem;
+}
+.custom-control:focus {
+  outline: none !important;
+}
+.assetImage {
+  max-height: 250px;
+}
+.assetImage-container {
+  height: 17rem;
+}
+.flex-container {
+  height: 300px;
+}
+
+@media screen and (min-width: 768px) {
+  .im {
+    min-width: 30rem;
+  }
+
+  .assetImage {
+    max-height: 600px;
+  }
+
+  .assetImage-container {
+    height: 50rem;
+  }
+
+  .flex-container {
+    height: 700px;
+  }
+}
+</style>
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,3 +1,89 @@
 <template>
-    <p>This is portfolio Page</p>
+  <div>
+    <div class="bg-gradient-to-br from-green-400 to-cyan-500">
+      <div
+        class="pt-20 pl-10 text-3xl font-bold md:text-6xl text-left underline"
+      >
+        Portfolio
+      </div>
+      <div
+        class="text-left pl-10 text-gray-500 md:text-right text-md md:text-2xl md:pr-10 pb-5"
+      >
+        Get in touch
+      </div>
+
+      <div v-for="data in projectData" :key="data.title" class="project">
+        <project :pData="data"/>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import Project from "../components/Portfolio/Project.vue";
+
+export default {
+  components: {
+    Project,
+  },
+  data: function () {
+    return {
+      projectData: [{
+        minTitle: "Built with Java",
+        title: "A program to manage your Mill2",
+        intro:
+          "2Utility classes help you work within the constraints of a system instead of littering your stylesheets with arbitrary values. They make it easy to be consistent with color choices, spacing, typography, shadows, and everything else that makes up a well-engineered design system",
+        assets: [
+          {
+            image: "src/assets/Portfolio/Project/CalendarMillJava/asset1.png",
+            desc: "21All the customers in your Mill/Company",
+          },
+          {
+            image: "src/assets/Portfolio/Project/CalendarMillJava/asset2.png",
+            desc: "21All the 32423s in your Mill/Company",
+          },
+          {
+            image: "src/assets/Portfolio/Project/CalendarMillJava/asset3.png",
+            desc: "21All the custom436346our Mill/Company",
+          },
+          {
+            image: "src/assets/Portfolio/Project/CalendarMillJava/asset4.png",
+            desc: "21All t345345mers in your Mill/Company",
+          },
+          {
+            image: "src/assets/Portfolio/Project/CalendarMillJava/asset5.png",
+            desc: "21All the customers in45645ill/Company",
+          },
+          {
+            image: "src/assets/Portfolio/Project/CalendarMillJava/asset6.png",
+            desc: "21All th34534ers in your Mill/Company",
+          },
+          {
+            image: "src/assets/Portfolio/Project/CalendarMillJava/asset7.png",
+            desc: "21All the customers i456457Mill/Company",
+          },
+        ],
+        contents:[]
+      },
+      {
+        minTitle: "Built with Java2",
+        title: "A program to manage your Mill",
+        intro:
+          "Utility classes help you work within the constraints of a system instead of littering your stylesheets with arbitrary values. They make it easy to be consistent with color choices, spacing, typography, shadows, and everything else that makes up a well-engineered design system",
+        assets: [
+          {
+            image: "src/assets/Portfolio/Project/CalendarMillJava/proj1.png",
+            desc: "331All the customers in your Mill/Company",
+          },
+          {
+            image: "src/assets/Portfolio/Project/CalendarMillJava/proj1.png",
+            desc: "332All the customers in your Mill/Company",
+          },
+        ],
+        contents:[]
+      }
+      ]
+    };
+  },
+};
+</script>
